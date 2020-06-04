01c76971ce3518ef2bbc305bf4436be4d5c9807e
KHB <tdotholla@users.noreply.github.com>
2020-06-04 12:04:31 -0400

    who knows...

--- imports/ui/components/mapFilter.js
+++ imports/ui/components/mapFilter.js
@@ -1,30 +1,35 @@
-import Categories from '/imports/startup/collections/categories';
-import Listings from '/imports/startup/collections/listings';
+import Categories from "/imports/startup/collections/categories";
+import Listings from "/imports/startup/collections/listings";
 
-import './mapFilter.html';
+import "./mapFilter.html";
 
-Template.mapFilter.onCreated(function () {
-  this.subscribe('categories');
-  this.subscribe('listings_locs');
+Template.mapFilter.onCreated(function() {
+  this.subscribe("categories");
+  this.subscribe("listings_locs");
 });
 
 Template.mapFilter.helpers({
-  categories: function () {
+  categories: function() {
     return Categories.find();
   },
   catCount: function(cat) {
-    return Listings.find({categories: {$elemMatch: {$in: [ cat ]}}}).count();
+    return Listings.find({
+      categories: { $elemMatch: { $in: [cat] } }
+    }).count();
   }
 });
 
 Template.mapFilter.events({
-  'click .switch label, click .main-switch label': function(event,templateInstance) {
+  "click .switch label, click .main-switch label": function(
+    event,
+    templateInstance
+  ) {
     event.preventDefault();
   },
-  'click .main-switch': function(event,templateInstance) {
+  "click .main-switch": function(event, templateInstance) {
     // STOP PROPAGATING SO DROPDOWN DOESNT CLOSE
     event.stopPropagation();
-    const elArray = $('#filter_dropdown .switch input');
+    const elArray = $("#filter_dropdown .switch input");
     // console.log(elArray);
 
     for (let i = 1, len = elArray.length; i < len; i++) {
@@ -37,27 +42,25 @@ Template.mapFilter.events({
         //show all markers
       }
     }
-
   },
-  'click .cat_item .switch': function(event,templateInstance) {
+  "click .cat_item .switch": function(event, templateInstance) {
     // event.preventDefault();
     event.stopPropagation();
     //INPUT ELEMENT
     const el = $(event.currentTarget.firstElementChild.children[0]);
-    
+
     if (event.currentTarget.title) {
       //make sure this runs only once!
       toggleGroup(event.currentTarget.title);
       // $('.dropdown-button').dropdown('close');
     }
 
-    if (el.prop('checked')) {
-      el.prop('checked', false);
-      el.css('background-color', '#e34');
+    if (el.prop("checked")) {
+      el.prop("checked", false);
+      el.css("background-color", "#e34");
     } else {
-      el.prop('checked', true);
-      el.css('background-color', '#3af');
+      el.prop("checked", true);
+      el.css("background-color", "#3af");
     }
-
   }
 });
